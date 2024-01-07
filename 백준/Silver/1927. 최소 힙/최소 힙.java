5871c9573ab1404e72c788430a1c123694e5213d
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-01-07 16:20:18 +0900

    [Silver II] Title: 최소 힙, Time: 272 ms, Memory: 24620 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        PriorityQueue<Integer> h = new PriorityQueue<Integer>();
+        for(int i = 0 ; i < n ; i++){
+            int x = Integer.parseInt(bufferedReader.readLine());
+            if(x == 0){
+                Integer answer = h.poll();
+                if (answer == null){
+                    stringBuilder.append("0\n");
+                }else{
+                    stringBuilder.append(answer.toString());
+                    stringBuilder.append("\n");
+                }
+            }else{
+                h.add(x);
+            }
+            
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
