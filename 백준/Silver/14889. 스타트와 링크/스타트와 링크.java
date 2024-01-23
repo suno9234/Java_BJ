dd71b8b1875c693ea8a644aa83f5c2718c71cde3
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-01-23 21:11:54 +0900

    [Silver I] Title: 스타트와 링크, Time: 276 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,93 +1,71 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static int n;
-    static int arr[][];
-    static int nums[];
-    static int nums2[];
-    static int firstC=0;
-    static int secondC=0;
-    static boolean visited[];
-    static int min = 2100000000;
-
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        arr = new int[n][n];
-        nums = new int[n / 2];
-        nums2 = new int[n/2];
-        visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = scan.nextInt();
+    static int [] party ;
+    static int [] party2;
+    static int answer = Integer.MAX_VALUE;
+    static int [][] _map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bufferedReader.readLine());
+        party = new int[n/2];
+        party2 = new int[n/2];
+        _map = new int[n][n];
+        for(int i = 0 ; i < n;i++){
+            String [] tokens = bufferedReader.readLine().split(" ");
+            for(int j = 0 ; j < n ; j++){
+                _map[i][j] = Integer.parseInt(tokens[j]);
             }
         }
-        func(0,0);
-        System.out.println(min);
+        back(0,n/2);
+        System.out.println(answer);
     }
-
-    static void func(int start, int counter) {
-        if (counter == n / 2) {
-            int calculated = calc();
-            if (calculated < min) {
-                min = calculated;
+    static void back(int start ,int cnt){
+        if(cnt == 0){
+            // party에 팀인 사람들 번호가 배열로 들어가있음.
+            int idx = 0;
+            for(int i = 0 ; i < n ; i++){
+                int flag = 0;
+                for(int j = 0 ; j < n/2;j++){
+                    if(party[j] == i){
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0){
+                    party2[idx++] = i;
+                }
             }
+            //System.out.println(Arrays.toString(party));
+            int temp = 0;
+            int temp2 = 0;
+            for(int i = 0 ; i < n/2;i++){
+                for(int j = i+1 ; j < n/2;j++){
+                    temp+= _map[party[i]][party[j]] + _map[party[j]][party[i]];
+                }
+            }
+            for(int i = 0 ; i < n/2;i++){
+                for(int j = i+1 ; j < n/2;j++){
+                    temp2+= _map[party2[i]][party2[j]] + _map[party2[j]][party2[i]];
+                }
+            }
+            if(answer > (int)Math.abs(temp-temp2)){
+                answer = (int)Math.abs(temp-temp2);
+            }
+
             return;
         }
-            for (int i = start; i < n; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    nums[counter] = i;
-                    func(i+1,counter + 1);
-                    visited[i] = false;
-                }
-            }
-
-        
-
+        for(int i = start ;i < n ; i++){
+            cnt--;
+            party[cnt] = i;
+            back(i+1,cnt);
+            cnt++;
+        }
     }
-    //nums[n/2] 에 내가 뽑은 숫자들이 들어있음
-    static int calc(){
 
-        int c=0;
-        for(int i =0;i<n;i++){
-            int idx = 0;
-            for(idx=0;idx<n/2;idx++){
-                if(nums[idx]==i){
-                    idx=0;
-                    break;
-                }
-            }
-            if(idx ==n/2){
-                nums2[c]=i;
-                c++;
-            }
-            
-
-        }
-        
-        
-        firstC=0;
-        for(int i=0;i<n/2-1;i++){
-            for(int j=i+1;j<n/2;j++){
-                firstC+=arr[nums[i]][nums[j]];
-                firstC+=arr[nums[j]][nums[i]];
-            }
-        }
-        secondC=0;
-        for(int i=0;i<n/2-1;i++){
-            for(int j=i+1;j<n/2;j++){
-                secondC+=arr[nums2[i]][nums2[j]];
-                secondC+=arr[nums2[j]][nums2[i]];
-            }
-        }
-        if(firstC-secondC<0){
-            return secondC-firstC;
-        }else{
-            return firstC-secondC;
-        }
-
-        
-    }    
 }
-    
