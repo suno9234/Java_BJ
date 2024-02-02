46d4bec51f71fb64bf2fb4a8178e1a3345f0bce3
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-02-02 16:32:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 120 ms, Memory: 20,928 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -2,37 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Solution{
-
 	static int n ,_min, _max;
-	static int[] nums;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine())+1;
 		int [] opers = new int[4];
 		for(int tc = 1 ; tc < T; tc++) {
 			_max = Integer.MIN_VALUE;
 			_min = Integer.MAX_VALUE;
 			n = Integer.parseInt(br.readLine());
-			
-			String [] tokens = br.readLine().split(" ");
-			for(int i = 0 ; i < 4 ; i++) {
-				opers[i] = Integer.parseInt(tokens[i]);
-			}
-			
-			nums = new int[n];
-			tokens = br.readLine().split(" ");
-			for(int i = 0 ; i < n ; i++) {
-				nums[i] = Integer.parseInt(tokens[i]);
-			}
-			
-			perm(0,nums[0],opers);
+			int [] nums = new int[n];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0 ; i < 4 ; i++) opers[i] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0 ; i < n ; i++) nums[i] = Integer.parseInt(st.nextToken());
+			perm(0,nums[0],opers,nums);
 			sb.append("#").append(tc).append(" ").append(_max-_min).append("\n");
-			
 		}
 		System.out.println(sb.toString());
 	}
-	static void perm(int cnt, int calc, int [] opers) {
+	static void perm(int cnt, int calc, int [] opers , int[] nums) {
 		if(++cnt == n) {
 			_max = Math.max(_max, calc);
 			_min = Math.min(_min, calc);
@@ -40,22 +31,22 @@ public class Solution{
 		}
 		if(opers[0] > 0) {
 			opers[0]--;
-			perm(cnt , calc + nums[cnt],opers);
+			perm(cnt , calc + nums[cnt],opers , nums);
 			opers[0]++;
 		}
 		if(opers[1] > 0) {
 			opers[1]--;
-			perm(cnt , calc - nums[cnt],opers);
+			perm(cnt , calc - nums[cnt],opers, nums);
 			opers[1]++;
 		}
 		if(opers[2] > 0) {
 			opers[2]--;
-			perm(cnt , calc * nums[cnt],opers);
+			perm(cnt , calc * nums[cnt],opers, nums);
 			opers[2]++;
 		}
 		if(opers[3] > 0) {
 			opers[3]--;
-			perm(cnt , calc / nums[cnt],opers);
+			perm(cnt , calc / nums[cnt],opers, nums);
 			opers[3]++;
 		}
 		cnt--;
