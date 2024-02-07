9955bc06cf4051e1c9a7b901a87a8bd7964d18f2
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-02-07 11:45:57 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,17 +1,17 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] tokens ;
         int [][] arr = new int[100][100];
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0 ; i< n; i++){
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            tokens = br.readLine().split(" ");
+            int x = Integer.parseInt(tokens[0]);
+            int y = Integer.parseInt(tokens[1]);
             for(int j = 0 ; j < 10; j++){
                 for(int k = 0 ; k<10; k++){
                     arr[x+j][y+k] = 1;
