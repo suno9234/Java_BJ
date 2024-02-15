57cc76d389deb037551b3c515c64a5ef08f68910
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-02-15 15:23:53 +0900

    [Gold III] Title: 사다리 조작, Time: 2652 ms, Memory: 15304 KB -BaekjoonHub

---
+++ 백준/Gold III/15684. 사다리 조작/사다리 조작.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n,m,h,answer=-1;
+    static int [][] _map;
+    public static void main(String[] args)  throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String [] tokens = bufferedReader.readLine().split(" ");
+        n = Integer.parseInt(tokens[0]);
+        m = Integer.parseInt(tokens[1]);
+        h = Integer.parseInt(tokens[2]);
+        _map = new int[h+1][n];
+
+        for(int i = 0 ; i < m ; i++){
+            tokens = bufferedReader.readLine().split(" ");
+            int a = Integer.parseInt(tokens[0])-1;
+            int b = Integer.parseInt(tokens[1])-1;
+            _map[a][b] = 1;
+            _map[a][b+1] = -1;
+        }
+
+        subs(0 ,0);
+        System.out.println(answer);
+    }
+    static void subs(int cnt ,int start){
+        if(cnt > 3){
+            return;
+        }else if(check()){
+            if(answer >= 0){
+                answer = Math.min(answer,cnt);
+            }else {
+                answer = cnt;
+            }
+            return ;
+        }
+        for(int i = start ; i < h*n; i++){
+            if(_map[i/n][i%n] == 0 && i%n < n-1  ){
+                // 현재 놓으려는 곳에 사다리가 없어야 하고 j의 범위는 가장 끝칸보다는 한칸 적어야 한다
+                if( i%n < n-2 && _map[i/n][i%n+1] != 0) continue;
+                // 사다리를 놓아 연결되는 칸에 사다리가 있는 경우는 제외해야 한다
+                _map[i/n][i%n]  =  1;
+                _map[i/n][i%n+1] = -1;
+                subs(cnt+1 ,i+1);
+                _map[i/n][i%n] = 0;
+                _map[i/n][i%n+1] = 0;
+            }
+
+        }
+    }
+    static boolean check(){
+        for(int i = 0 ; i < n; i++){
+            int x = 0, y= i;
+            while(x < h+1){
+                if(_map[x][y] > 0 ){
+                    y++;
+                }else if(_map[x][y] < 0 ){
+                    y--;
+                }
+                x++;
+            }
+            if (y != i){
+                return false;
+            }
+        }
+        return true;
+    }
+}
