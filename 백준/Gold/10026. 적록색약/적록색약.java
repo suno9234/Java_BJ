2526e3580524aa16b1efd027bec03c24d05fa42b
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-02-20 15:16:04 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 12560 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,99 +1,78 @@
+import java.util.*;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+public class Main{
 
-public class Main {
+	static int n;
+	static char[][] _map;
+	static int[][] group;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
 
-    static boolean [][] _visited;
-    static char [][] arr;
-    static char [][] arr1;
-    static int [] dx = {0,0,1,-1};
-    static int [] dy = {1,-1,0,0};
-    static int n = 0;
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(bufferedReader.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		_map = new char[n][n];
+		group = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			String nl = br.readLine();
+			for (int j = 0; j < n; j++) {
+				_map[i][j] = nl.charAt(j);
+			}
+		}
+		int idx = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (group[i][j] == 0) {
+					dfs(i, j, idx++, _map[i][j]);
+				}
+			}
+		}
 
-        int answer0 = 0;
-        int answer1 =0;
-        arr = new char[n][n];
-        arr1  = new char[n][n];
-        _visited = new boolean[n][n];
+		sb.append(idx-1).append(" ");
 
-        for(int i = 0; i<n; i++){
-            String nl = bufferedReader.readLine();
-            for(int j = 0 ; j<n; j++){
-                arr[i][j] = nl.charAt(j);
-                arr1[i][j] = nl.charAt(j);
-                if(nl.charAt(j) == 'G'){
-                    arr1[i][j] = 'R';
-                }
-            }
-        }
+		idx = 1;
+		group = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (group[i][j] == 0) {
+					dfsRG(i, j, idx++, _map[i][j]);
+				}
+			}
+		}
+		sb.append(idx-1);
+		System.out.println(sb.toString());
+	}
 
-        for(int i = 0 ; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!_visited[i][j]) {
-                    //System.out.printf("%d %d\n",i,j);
-                    bfs(i,j,arr[i][j]);
-                    answer0+=1;
-                }
-            }
-        }
-        _visited = new boolean[n][n];
-        for(int i = 0 ; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!_visited[i][j]) {
-                    //System.out.printf("%d %d\n",i,j);
-                    bfs2(i,j,arr1[i][j]);
-                    answer1+=1;
-                }
-            }
-        }
-        System.out.printf("%d %d",answer0,answer1);
+	static void dfs(int x, int y, int idx, char color) {
+		group[x][y] = idx;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < n && ny < n && _map[nx][ny] == color && group[nx][ny] == 0) {
+				dfs(nx, ny, idx, color);
+			}
+		}
+	}
 
-    }
-    public static void bfs(int x, int y, char type){
-        _visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[]{x,y});
-        while(!queue.isEmpty()){
-            int [] now = queue.poll();
-            for(int i = 0 ; i < 4 ; i++){
-                int nx = now[0]+dx[i];
-                int ny = now[1]+dy[i];
-                if(nx >=0 && ny >=0 && nx < n && ny < n && arr[nx][ny] == type && !_visited[nx][ny] ){
-                    _visited[nx][ny] = true;
-                    queue.add(new int[]{nx,ny});
-                }
-            }
-        }
-    }
-    public static void bfs2(int x, int y, char type){
-        _visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[]{x,y});
-        while(!queue.isEmpty()){
-            int [] now = queue.poll();
-            for(int i = 0 ; i < 4 ; i++){
-                int nx = now[0]+dx[i];
-                int ny = now[1]+dy[i];
-                if(type == 'B') {
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr1[nx][ny] == type && !_visited[nx][ny]) {
-                        _visited[nx][ny] = true;
-                        queue.add(new int[]{nx, ny});
-                    }
-                }else{
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr1[nx][ny] != 'B' && !_visited[nx][ny]) {
-                        _visited[nx][ny] = true;
-                        queue.add(new int[]{nx, ny});
-                    }
-                }
-            }
-        }
-    }
+	static void dfsRG(int x, int y, int idx, char color) {
+		group[x][y] = idx;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < n && ny < n && group[nx][ny] == 0) {
+				if (color == 'B') {
+					if (_map[nx][ny] == 'B') {
+						dfsRG(nx, ny, idx, color);
+					}
+				} else if(_map[nx][ny]!= 'B'){
+					dfsRG(nx,ny,idx,color);
+				}
+
+			}
+		}
+	}
+
 }
