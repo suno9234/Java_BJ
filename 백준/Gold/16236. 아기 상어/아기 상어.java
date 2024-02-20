5add2d66f0a2e7cd802248f9a0b0953cd0b7c4a9
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-02-20 09:35:16 +0900

    [Gold III] Title: 아기 상어, Time: 88 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
         int n = Integer.parseInt(bufferedReader.readLine());
@@ -32,26 +30,35 @@ public class Main{
         boolean isNext = true;
         while(isNext){
             boolean [][] _visited = new boolean[n][n];
-            Queue<Coor> queue = new LinkedList<Coor>();
-            //System.out.println("Coordinate: "+posX+" "+posY+" SIZE : "+sizeOfShark+" DIST : "+answer);
-            queue.add(new Coor(posX, posY));
+            PriorityQueue <int []> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            	public int compare(int [] ia1, int [] ia2) {
+            		return ia1[0]-ia2[0]==0 ? ia1[1]-ia2[1] : ia1[0]-ia2[0];
+            	}
+            });
+            pq.add(new int[] {posX,posY});
             int flag = 0;
             int distance = 0;
-            while (flag == 0 && !queue.isEmpty()) {
+            while (flag == 0 && !pq.isEmpty()) {
                 // 더이상 갈곳이 없을 때 종료
-                LinkedList<Coor> _nextQueue = new LinkedList<>();
+                PriorityQueue<int[]> nextPq = new PriorityQueue<>(new Comparator<int[]>() {
+                	public int compare(int [] ia1, int [] ia2) {
+                		return ia1[0]-ia2[0]==0 ? ia1[1]-ia2[1] : ia1[0]-ia2[0];
+                	}
+                });
                 //현재 위치에서 탐색
-                while(!queue.isEmpty()){
-                    Coor now = queue.poll();
-                    if(_map[now.x][now.y] > 0 && _map[now.x][now.y] < sizeOfShark){
+                while(!pq.isEmpty()){
+                    int [] now = pq.poll();
+                    int x = now[0];
+                    int y = now[1];
+                    if(_map[x][y] > 0 && _map[x][y] < sizeOfShark){
                         // queue 에서 뽑은게 내가 먹을 수 있는 물고기라면
-                        posX = now.x;
-                        posY = now.y;
-                        _map[now.x][now.y] = 0;
-                        food +=1;
+                        posX = x;
+                        posY = y;
+                        _map[x][y] = 0;
+                        food ++;
                         if (food == sizeOfShark) {
                             food = 0;
-                            sizeOfShark += 1;
+                            sizeOfShark++;
                         }
                         flag = 1;
                         answer+=distance;
@@ -59,29 +66,17 @@ public class Main{
                     }
                     for (int i = 0; i < 4; i++) {
                         // 상하좌우에 대해서
-                        int nx = now.x + dx[i];
-                        int ny = now.y + dy[i];
+                        int nx = x + dx[i];
+                        int ny = y + dy[i];
                         if (nx >= 0 && ny >= 0 && nx < n && ny < n && _map[nx][ny] <= sizeOfShark && !_visited[nx][ny]) {
                             _visited[nx][ny] = true;
-                            _nextQueue.add(new Coor(nx, ny));
+                            nextPq.add(new int[] {nx, ny});
                         }
                     }
 
                 }
-                //System.out.println();
-                _nextQueue.sort(new Comparator<Coor>() {
-                    @Override
-                    public int compare(Coor a, Coor b) {
-                        if (a.x == b.x) {
-                            return a.y - b.y;
-                        }
-                        return a.x - b.x;
-
-                    }
-                });
-
-                queue.addAll((Queue<Coor>)_nextQueue);
-                distance+=1;
+                pq = nextPq;
+                distance++;
             }
             if(flag == 0){
                 isNext = false;
@@ -89,12 +84,5 @@ public class Main{
         }
         System.out.println(answer);
     }
-    static class Coor{
-        int x;
-        int y;
-        public Coor(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+ 
 }
