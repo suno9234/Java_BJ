1265e07519a1e9b7c633254fd9e44ef0a0e8a570
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-03-02 18:39:37 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 184 ms, Memory: 26920 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -1,76 +1,78 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n,m;
-	static int [][] A;
-	static ArrayList<Integer> cloud = new ArrayList<>();
-	static int [] dx = {0,-1,-1,-1,0,1,1,1};
-	static int [] dy = {-1,-1,0,1,1,1,0,-1};
-	static int [] xDx = {1,1,-1,-1};
-	static int [] xDy = {1,-1,1,-1};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String [] tokens = br.readLine().split(" ");
-		n = Integer.parseInt(tokens[0]);
-		m = Integer.parseInt(tokens[1]);
-		A = new int[n][n];
-		cloud.add((n-1)*n);
-		cloud.add((n-1)*n+1);
-		cloud.add((n-2)*n);
-		cloud.add((n-2)*n+1);
-		for(int i = 0 ; i < n ; i++) {
-			tokens = br.readLine().split(" ");
-			for(int j = 0 ; j < n ; j++) {
-				A[i][j] = Integer.parseInt(tokens[j]);
-			}
-		}
-		// 100*2500*
-		for(int magic = 0 ; magic < m ; magic++) {
-			tokens = br.readLine().split(" ");
-			int d = Integer.parseInt(tokens[0])-1;
-			int s = Integer.parseInt(tokens[1]);
-			ArrayList <Integer> newCloud = new ArrayList<>();
-			
-			for(Integer cl : cloud) {
-				newCloud.add(((cl/n+s*dx[d]+n*100)%n)*n +(cl%n+s*dy[d]+n*100)%n);
-			}
-			for(Integer cl : newCloud) {
-				A[cl/n][cl%n] +=1;
-			}
-			for(Integer cl : newCloud) {
-				int temp = 0;
-				for(int i = 0 ; i< 4 ; i++) {
-					int nx = cl/n+xDx[i];
-					int ny = cl%n+xDy[i];
-					if(nx >=0 && ny >= 0 && nx < n && ny <n && A[nx][ny]>0) {
-						temp++;
-					}
-				}
-				A[cl/n][cl%n] += temp;
-			}
-			ArrayList <Integer> returnCloud = new ArrayList<>();
-			for(int i = 0 ; i < n ; i++) {
-				for(int j = 0 ; j < n ; j++) {
-					if(A[i][j] >= 2 && !newCloud.contains(i*n+j) && ! returnCloud.contains(i*n+j) ) {
-						A[i][j] -= 2;
-						returnCloud.add(i*n+j);
-						
-					}
-				}
-			}
-			cloud = returnCloud;
-		}
-		int answer = 0;
-		for(int i = 0 ; i < n ; i++) {
-			for(int j = 0 ; j < n ; j++) {
-				answer+= A[i][j];
-			}
-		}
-		System.out.println(answer);
-		
-	}
+    static int n,m;
+    static int [][] A;
+    static ArrayDeque<int [] > cloud = new ArrayDeque<>();
+    static int [] dx = {0,-1,-1,-1,0,1,1,1};
+    static int [] dy = {-1,-1,0,1,1,1,0,-1};
+    static int [] xDx = {1,1,-1,-1};
+    static int [] xDy = {1,-1,1,-1};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] tokens = br.readLine().split(" ");
+        n = Integer.parseInt(tokens[0]);
+        m = Integer.parseInt(tokens[1]);
+        A = new int[n][n];
+        cloud.add(new int[]{n-1,0});
+        cloud.add(new int[]{n-1,1});
+        cloud.add(new int[]{n-2,0});
+        cloud.add(new int[]{n-2,1});
+        for(int i = 0 ; i < n ; i++) {
+            tokens = br.readLine().split(" ");
+            for(int j = 0 ; j < n ; j++) {
+                A[i][j] = Integer.parseInt(tokens[j]);
+            }
+        }
+
+        for(int magic = 0 ; magic < m ; magic++) {
+            tokens = br.readLine().split(" ");
+            int d = Integer.parseInt(tokens[0])-1;
+            int s = Integer.parseInt(tokens[1]);
+            ArrayDeque <int[]> newCloud = new ArrayDeque<>();
+            boolean [][] v = new boolean[n][n];
+            while(!cloud.isEmpty()) {
+                int [] cl = cloud.poll();
+                int x = cl[0];
+                int y = cl[1];
+                int nx = (x+dx[d]*s + n*100)%n;
+                int ny = (y+dy[d]*s + n*100)%n;
+                newCloud.add(new int[]{nx,ny});
+                A[nx][ny]++;
+                v[nx][ny] = true;
+            }
+            while(!newCloud.isEmpty()){
+                int [] now = newCloud.poll();
+                int x = now[0];
+                int y = now[1];
+                for(int i = 0 ; i< 4 ; i++) {
+                    int nx = x+xDx[i];
+                    int ny = y+xDy[i];
+                    if(nx >=0 && ny >= 0 && nx < n && ny <n && A[nx][ny]>0) {
+                        A[x][y] ++;
+                    }
+                }
+            }
+            ArrayDeque <int []> returnCloud = new ArrayDeque<>();
+            for(int i = 0 ; i < n ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    if(A[i][j] >= 2 && !v[i][j] ) {
+                        A[i][j] -= 2;
+                        returnCloud.add(new int[]{i,j});
+                    }
+                }
+            }
+            cloud = returnCloud;
+        }
+        int answer = 0;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                answer+= A[i][j];
+            }
+        }
+        System.out.println(answer);
+
+    }
 
 }
