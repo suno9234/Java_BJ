6e7ca9fc50b2aa2b614b0edad57e7cdb21be5e61
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-03-13 10:55:10 +0900

    [Bronze V] Title: 새싹, Time: 68 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Main{
+    public static void main(String [] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n").append("r`-_   ,'  ,/\n").append(" \\. \". L_r'\n").append("   `~\\/\n").append("      |\n").append("      |");
+        System.out.println(sb.toString());
+    }
+}
