ace863d1d23fffef6de11d1ecd567f334010fed4
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-03-13 15:30:08 +0900

    [Gold I] Title: 도로검문, Time: 632 ms, Memory: 63168 KB -BaekjoonHub

---
+++ 백준/Gold/2307. 도로검문/도로검문.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m, answer;
+	static int initDist;
+	static int[] parent;
+	static ArrayList<int []> road;
+	static List<int[]>[] graph;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] tokens = br.readLine().split(" ");
+		n = Integer.parseInt(tokens[0]);
+		m = Integer.parseInt(tokens[1]);
+		graph = new List[n + 1];
+		for (int i = 1; i < n + 1; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < m; i++) {
+			tokens = br.readLine().split(" ");
+			int a = Integer.parseInt(tokens[0]);
+			int b = Integer.parseInt(tokens[1]);
+			int t = Integer.parseInt(tokens[2]);
+			graph[a].add(new int[] { b, t });
+			graph[b].add(new int[] { a, t });
+		}
+		road = new ArrayList<>();
+		parent = new int[n + 1];
+		initDist = dijkstra(new int[] { -1, -1 });
+		int node = n;
+		while(node > 1) {
+			road.add(new int[] {node,parent[node]});
+			int result = dijkstra(new int[] {node,parent[node]});
+			if(result == -1) {
+				// 끝까지 못가는 경우
+				System.out.println(-1);
+				return;
+			}else {
+				answer = Math.max(answer, result-initDist);
+			}
+			node = parent[node];
+		}
+		System.out.println(answer);
+		
+	}
+
+	static int dijkstra(int[] deleted) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1], o2[1]));
+		boolean[] v = new boolean[n + 1];
+		int[] minDist = new int[n + 1];
+		Arrays.fill(minDist, Integer.MAX_VALUE);
+		minDist[1] = 0;
+		pq.add(new int[] { 1, 0,1 });
+		int last = 1;
+		while (!pq.isEmpty()) {
+			int[] now = pq.poll();
+			int pos = now[0];
+			int dist = now[1];
+			int start = now[2];
+			if (v[pos])
+				continue;
+			if (pos == n) {
+				return minDist[n];
+			}
+			v[pos] = true;
+			for (int[] next : graph[pos]) {
+				int np = next[0];
+				int nd = next[1];
+				if (!v[np] && minDist[np] > dist + nd && !(pos == deleted[0] && np == deleted[1]) && !(pos == deleted[1] && np == deleted[0])) {
+					if(deleted[0] == -1) 
+						parent[np] = pos;
+					minDist[np] = dist + nd;
+					pq.add(new int[] { np, dist + nd,pos });
+				}
+			}
+		}
+		return -1;
+	}
+}
