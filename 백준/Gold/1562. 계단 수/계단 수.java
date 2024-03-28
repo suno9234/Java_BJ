f35ad356152cb599537955ff3bdaf41a6dde7157
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-03-29 08:54:39 +0900

    [Gold I] Title: 계단 수, Time: 120 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	static int n;
+    static int [][][] dp;
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int [n+1][10][(1<<10)];
+        //              i길이의 j로 끝나는 지금까지 v들을 방문한 계단수의 개수
+        for(int i = 1 ; i < 10 ; i++) {
+        	dp[1][i][1<<i] = 1;
+        }
+        for(int i = 1 ; i < n ; i++) {
+        	for(int j = 0 ; j < 10 ; j++) {
+        		for(int v = 0 ; v < (1<<10) ; v++) {
+        			// dp[i][j][v방문추가]
+        			// 현재 = dp[i][j][v]
+        			if(j+1 < 10) {
+        				dp[i+1][j+1][v | (1<<(j+1))] = (dp[i+1][j+1][v | (1<<(j+1))] + dp[i][j][v])%1_000_000_000;
+        				//System.out.printf("dp[%d][%d][%d] : %d\n",i+1,j+1,v,dp[i+1][j+1][v | (1<<(j+1))]);
+        			}
+        			if(j-1 >= 0) {
+        				dp[i+1][j-1][v | (1<<(j-1))] = (dp[i+1][j-1][v | (1<<(j-1))] + dp[i][j][v])%1_000_000_000;
+        				//System.out.printf("dp[%d][%d][%d] : %d\n",i+1,j-1,v,dp[i+1][j-1][v | (1<<(j-1))]);
+        			}
+        		}
+        	}
+        }
+        
+        int answer = 0;
+        for(int i = 0 ; i < 10 ; i++) {
+        	answer = (answer+dp[n][i][(1<<10) -1])%1_000_000_000;
+        }
+        System.out.println(answer);
+        br.close();
+	}
+}
