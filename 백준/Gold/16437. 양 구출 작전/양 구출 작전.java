8f4108a9933e7a1a59a33313a603438a5d63c957
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-04-03 15:32:50 +0900

    [Gold III] Title: 양 구출 작전, Time: 588 ms, Memory: 72908 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main{
 	static int n;
 	static int [] in;
-	static long [][] info;
+	static int [] nexts;
+	static long [] info;
     public static void main(String[] args) throws Exception {
 //    	System.setIn(new FileInputStream("res/input_d2_2001.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +13,8 @@ public class Main{
         StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(br.readLine());
         in = new int[n+1];
-        info = new long[n+1][2];
+        nexts = new int[n+1];
+        info = new long[n+1];
         for(int i = 0 ; i < n-1 ; i++) {
         	tokens = br.readLine().split(" ");
         	String type = tokens[0];
@@ -20,32 +22,33 @@ public class Main{
         	int next = Integer.parseInt(tokens[2]);
         	in[next]++;
         	if(type.equals("S")) {
-        		info[i+2] = new long[] {count,next};
+        		info[i+2] = count;
         	}else {
-        		info[i+2] = new long[] {-count,next}; 
+        		info[i+2] = -count; 
         	}
+        	nexts[i+2] = next;
         }
-        ArrayDeque<long []> queue = new ArrayDeque<>();
-        for(int i =1 ; i < n+1 ; i++) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for(int i =2 ; i < n+1 ; i++) {
         	if(in[i] == 0) {
-        		queue.add(info[i]);
+        		queue.add(i);
         	}
         }
         while(!queue.isEmpty()) {
-        	long [] now = queue.poll();
+        	int now = queue.poll();
         	// now = {count , next}
-        	long count = now[0];
-        	int next = (int)now[1];
+        	long count = info[now];
+        	int next = nexts[now];
         	in[next]--;
         	if(count > 0) {
         		// 양일 경우만 진행
-        		info[next][0]+=count;
+        		info[next]+=count;
         	}
         	if(in[next] == 0) {
-        		queue.add(info[next]);
+        		queue.add(next);
         	}
         }
-        System.out.println(info[1][0]);
+        System.out.println(info[1]);
         br.close();
 	}
 }
