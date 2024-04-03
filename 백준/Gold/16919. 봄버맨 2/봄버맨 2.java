a55754a025dd3ae141311f9a9225cacd53ca4c42
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-04-03 13:51:26 +0900

    [Gold III] Title: 봄버맨 2, Time: 104 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/16919. 봄버맨 2/봄버맨 2.java
+++ 백준/Gold/16919. 봄버맨 2/봄버맨 2.java
@@ -1,20 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
 	static int r, c, n;
 	static char[][][] map;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 //    	System.setIn(new FileInputStream("res/input_d2_2001.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tokens;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		tokens = br.readLine().split(" ");
 
-		r = Integer.parseInt(tokens[0]);
-		c = Integer.parseInt(tokens[1]);
-		n = Integer.parseInt(tokens[2]);
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
 		map = new char[r][c][6];
 		for (int i = 0; i < r; i++) {
 			String s = br.readLine();
@@ -24,40 +25,8 @@ public class Main{
 			}
 		}
 
-		for (int i = 2; i < 6; i++) {
-			for (int j = 0; j < r; j++) {
-				for (int k = 0; k < c; k++) {
-					map[j][k][i] = map[j][k][i-1];
-				}
-			}
-			if (i % 2 == 0) {
-				for (int j = 0; j < r; j++) {
-					for (int k = 0; k < c; k++) {
-						map[j][k][i] = 'O';
-					}
-				}
-			} else {
-				for (int j = 0; j < r; j++) {
-					for (int k = 0; k < c; k++) {
-						if (map[j][k][i - 3] == 'O' && map[j][k][i - 2] == 'O') {
-							if (k + 1 < c)
-								map[j][k + 1][i] = '.';
-							if (k - 1 >= 0)
-								map[j][k - 1][i] = '.';
-							if (j - 1 >= 0)
-								map[j - 1][k][i] = '.';
-							if (j + 1 < r)
-								map[j + 1][k][i] = '.';
-							map[j][k][i] = '.';
-						}
-
-					}
-				}
-
-			}
-
-		}
 		if(n == 1) {
+			// n == 1인경우 그냥 출력
 			for(int i = 0 ; i < r ; i++) {
 				for(int j = 0 ; j < c ; j++) {
 					sb.append(map[i][j][1]);
@@ -65,6 +34,38 @@ public class Main{
 				sb.append("\n");
 			}
 		}else {
+			
+			for (int i = 2; i < 6; i++) {
+				if (i % 2 == 0) {
+					for (int j = 0; j < r; j++) {
+						for (int k = 0; k < c; k++) {
+							map[j][k][i] = 'O';
+						}
+					}
+				} else {
+					for (int j = 0; j < r; j++) {
+						for (int k = 0; k < c; k++) {
+							if(map[j][k][i] == '\0') {
+								map[j][k][i] = map[j][k][i-1];
+							}
+							if (map[j][k][i - 3] == 'O' && map[j][k][i - 2] == 'O') {
+								if (k + 1 < c)
+									map[j][k + 1][i] = '.';
+								if (k - 1 >= 0)
+									map[j][k - 1][i] = '.';
+								if (j - 1 >= 0)
+									map[j - 1][k][i] = '.';
+								if (j + 1 < r)
+									map[j + 1][k][i] = '.';
+								map[j][k][i] = '.';
+							}
+
+						}
+					}
+
+				}
+
+			}
 			int modN = n%4;
 			if (modN == 0) modN = 4;
 			if(modN == 1) modN = 5;
@@ -75,10 +76,6 @@ public class Main{
 				sb.append("\n");
 			}
 		}
-
-		
-
 		System.out.println(sb);
-		br.close();
 	}
 }
