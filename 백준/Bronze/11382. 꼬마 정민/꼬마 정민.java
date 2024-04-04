fcbeb6e17683033eb7eb6cec20fd2494a272c36c
suno9234 <74644225+suno9234@users.noreply.github.com>
2024-04-04 15:37:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,3 @@
+import java.io.*;
+import java.util.*;
+class Main{public static void main(String [] args)throws Exception{BufferedReader br = new BufferedReader(new InputStreamReader(System.in));String [] tokens = br.readLine().split(" "); System.out.println(Long.parseLong(tokens[0])+Long.parseLong(tokens[1])+Long.parseLong(tokens[2]));}}
